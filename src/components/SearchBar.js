1cc3b35ce569d92cb7864834c9678bd6dc14cf5e
junlongtao <zeus@zhichou.com>
2017-12-04 18:37:41 +0800

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,17 +8,27 @@ export default class SearchBar extends React.Component {
     static defaultProps = { 
         prefix: 'zui',
         className: '',
+        onClick: ()=>{},
+        onFocus: ()=>{},
+        onChange: ()=>{},
         placeholder: '请输入',
-        onChange: ()=>{}
     } 
 
     render = () => {
         const prefix = this.props.prefix
         return <div className={prefix+'-search-bar '+this.props.className}>
             <Icon type="sousuo" className={prefix+'-search-bar-icon'}/>
-            <input type="text" value={this.props.value} className={prefix+'-search-bar-input'} placeholder={this.props.placeholder} onChange={(e)=>{
-                this.props.onChange(e.target.value)
-            }}/>
+            <input 
+                type="text" 
+                value={this.props.value} 
+                className={prefix+'-search-bar-input'} 
+                placeholder={this.props.placeholder} 
+                onClick={this.props.onClick}
+                onFocus={this.props.onFocus}
+                onChange={(e)=>{
+                    this.props.onChange(e.target.value)
+                }}
+            />
         </div>
     }
 }
